c69383a37896780863c1d837a575dcbe701e798d
Manoj Kumar <Manoj_Kumar2@jabil.com>
2025-02-10 21:52:18 +0530

    updated employee detailed view logic with latest modifications

--- org_trees/tree/urls.py
+++ org_trees/tree/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import EmployeeListView, EmployeeCreateView, EmployeeUpdateView, EmployeeDeleteView
+from .views import EmployeeListView, EmployeeCreateView, EmployeeUpdateView, EmployeeDeleteView,EmployeeDetailView
 
 urlpatterns = [
     path('employees/', EmployeeListView.as_view(), name='employee-list'),
     path('employees/create/', EmployeeCreateView.as_view(), name='employee-create'),
     path('employees/<int:pk>/update/', EmployeeUpdateView.as_view(), name='employee-update'),
     path('employees/<int:pk>/delete/', EmployeeDeleteView.as_view(), name='employee-delete'),
+    path('employees/<int:pk>/', EmployeeDetailView.as_view(), name='employee-detail'),
 ]
+
--- org_trees/tree/views.py
+++ org_trees/tree/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Employee
 from .serializers import EmployeeSerializer
+from rest_framework.generics import RetrieveAPIView
 
 # Existing EmployeeListView (GET method)
 class EmployeeListView(APIView):
@@ -10,7 +11,11 @@ class EmployeeListView(APIView):
         root_nodes = Employee.objects.filter(parent=None)  # Fetch only root nodes (e.g., "John")
         serializer = EmployeeSerializer(root_nodes, many=True)
         return Response(serializer.data)
-
+    
+class EmployeeDetailView(RetrieveAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    
 # New EmployeeCreateView (POST method for creating employees)
 class EmployeeCreateView(APIView):
     def post(self, request):
