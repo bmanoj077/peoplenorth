f838dd4b6dcae4aae3a3519f832d189dbe3b5a9a
Manoj Kumar <Manoj_Kumar2@jabil.com>
2025-02-10 20:28:30 +0530

    removed old files

--- org_trees/tree/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Employee(models.Model):
-    name = models.CharField(max_length=100)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
-
-    def __str__(self):
-        return self.name
--- org_trees/tree/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import EmployeeListView
-
-urlpatterns = [
-    path('employees/', EmployeeListView.as_view(), name='employee-list'),
-]
